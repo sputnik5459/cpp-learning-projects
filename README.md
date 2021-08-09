0dec451ee3ae3823555cffc4da49240875d46b23
sputnik5459 <sputnik5459@gmail.com>
2021-08-09 23:37:00 +0300

    sudoku: make window generation and default event listener, add readme, add todoes

---
+++ README.md
@@ -0,0 +1,11 @@
+## Sudoku
+
+Goals
+
+- make sudoku game without step-by-step tutorials
+- learn some cpp basics and SDK using
+- learn basics of SDL (https://www.libsdl.org/)
+
+Summary
+
+-
--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -1,17 +1,8 @@
+#include <SDL2/SDL.h>
+#include <SDL_ttf>
+
 #include <iostream>
 
-
-void print_game_map(int game_map[9][9]) {
-    // TODO: beautify
-    for (int row = 0; row < 9; row++) {
-        for (int col = 0; col < 9; col++) {
-            std::cout << game_map[row][col];
-            if (col == 2 || col == 5) std::cout << "|";
-        }
-        if (row == 2 || row == 5) std::cout << "\n-----------" << std::endl;
-        else std::cout << "\n";
-    }
-}
 
 int main() {
     // TODO: make sudoku maps reading from files
@@ -28,6 +19,76 @@ int main() {
             {3, 4, 9, 0, 0, 1, 6, 7, 2},
             {0, 5, 6, 0, 7, 3, 0, 8, 0}
     };
-    print_game_map(game_map);
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Sudoku", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+
+    if (win == nullptr){
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr){
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    //this opens a font style and sets a size
+    TTF_Font* Sans = TTF_OpenFont("Sans.ttf", 24);
+
+    // this is the color in rgb format,
+    // maxing out all would give you the color white,
+    // and it will be your text's color
+    SDL_Color White = {255, 255, 255};
+
+    // as TTF_RenderText_Solid could only be used on
+    // SDL_Surface then you have to create the surface first
+    SDL_Surface* surfaceMessage =
+            TTF_RenderText_Solid(Sans, "put your text here", White);
+
+    // now you can convert it into a texture
+    SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    SDL_Rect Message_rect; //create a rect
+    Message_rect.x = 0;  //controls the rect's x coordinate
+    Message_rect.y = 0; // controls the rect's y coordinte
+    Message_rect.w = 100; // controls the width of the rect
+    Message_rect.h = 100; // controls the height of the rect
+
+    // (0,0) is on the top left of the window/screen,
+    // think a rect as the text's box,
+    // that way it would be very simple to understand
+
+    // Now since it's a texture, you have to put RenderCopy
+    // in your game loop area, the area where the whole code executes
+
+    // you put the renderer's name first, the Message,
+    // the crop size (you can ignore this if you don't want
+    // to dabble with cropping), and the rect which is the size
+    // and coordinate of your texture
+    SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+
+
+    bool quitGameLoop = false;
+    while(!quitGameLoop) {
+        SDL_Event ev;
+
+        if(SDL_PollEvent(&ev) == 0) {
+            SDL_Delay(1);
+            continue;
+        }
+
+        switch(ev.type) {
+            case SDL_KEYDOWN:
+                quitGameLoop = true;
+        }
+    }
+
+    SDL_Quit();
     return 0;
 }
